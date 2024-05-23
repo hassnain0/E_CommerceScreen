036ba50ea61c29add9d49f0f3628c29429d45c09
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-05-23 17:09:32 +0500

    IntialCommit

---
+++ Screens/Favourite.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View  } from "react-native";
+
+const Favourite=()=>{
+<View>
+    
+</View>
+}
+
+export default Favourite;
---
+++ Screens/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FlatList, ImageBackground, StyleSheet, Text, Touchable, TouchableOpacity, View } from "react-native";
+
+function Home(): React.JSX.Element {
+
+  return (
+    <ImageBackground source={require('../assets/Banner.png')} style={styles.background}>
+      <View style={{ flex: 1, flexDirection: 'column', marginTop: 250, left: 20, }}>
+        <Text style={styles.TextContainer}>Fashion</Text>
+        <Text style={styles.TextContainer}>sale</Text>
+        <TouchableOpacity style={{ backgroundColor: '#D93022', width: 180, borderRadius: 20, height: 40, }}>
+          <Text style={{ fontSize: 18, color: 'white', textAlign: 'center', fontWeight: '400', top: 5, }}>Check</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ flex: 1, backgroundColor: 'white', }}>
+        <Text style={{ fontWeight: '700', fontSize: 40, color: 'black', left: 20 }}>New</Text>
+        <Text style={{ left: 12, fontWeight: '300', left: 20 }}>You've never seen it before!</Text>
+        <FlatList />
+      </View>
+
+    </ImageBackground>
+  );
+}
+const styles = StyleSheet.create({
+  background: {
+    width: '100%',
+    height: '100%',
+  },
+  TextContainer: {
+    color: 'white', fontSize: 45, fontWeight: '900',
+  },
+});
+
+export default Home;
